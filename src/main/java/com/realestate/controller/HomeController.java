736d2364c5300bb2bcb05432fe97115518972078
sthaaruchi <st120807@ait.ac.th>
2020-04-19 08:59:15 +0545

    Added log4j configuration

--- src/main/java/com/realestate/controller/HomeController.java
+++ src/main/java/com/realestate/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.realestate.controller;
 
 import java.security.Principal;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +11,14 @@ import com.realestate.dao.ReUserJPADao;
 
 @Controller
 public class HomeController {
+	private Logger logger = Logger.getLogger(HomeController.class);
+	
 	@Autowired 
 	ReUserJPADao userDao;
 	
 	@RequestMapping(path = "/home")
 	public String userDashboard(Principal request) {
+		logger.info("Displaying Homepage - userDashBoard()");
 		return "Home";
 	}
 	
